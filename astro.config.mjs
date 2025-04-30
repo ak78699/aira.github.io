d2ef3af3f1d1984089207e5408284083a9d0ebc1
Aira <ak7869@nyu.edu>
2025-04-30 12:15:28 +0400

    Moved dist contents to docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://ak78699.github.io',
 	base: '/aira.github.io/',
 	integrations: [tailwind()],
 });
